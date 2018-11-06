c01202d8c2c82d03caf4661d822ef951d65878e1
ashwani <asashwanishakya@gmail.com>
2018-11-06 12:13:42 +0530

    databse settings for pythonanywhere

--- multiuser_app/settings.py
+++ multiuser_app/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'o&d_1p0-wd@*co2ccvjxqom-rzk0c%wy%ite0jd$2ayt(#&dg='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ashwani64.pythonanywhere.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
